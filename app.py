36a8be7a4b8d8cc2ad7c749f20faa53de6c8d0fa
hartmannlars <hartmannlars@icloud.com>
2024-05-04 23:39:01 +0200

    bugfix

--- app.py
+++ app.py
@@ -3,38 +3,32 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-model_path = "sentinel_classifier_model.keras"
+# Load the model
+model_path = "sentinel_classificatiion_model.keras"
 model = tf.keras.models.load_model(model_path)
 
-
+# Define labels
 labels = ['AnnualCrop', 'Forest', 'HerbaceousVegetation', 'Highway', 'Industrial', 'Pasture', 'PermanentCrop', 'Residential', 'River', 'SeaLake']
-    
+
 def predict_image(image):
+    image = Image.fromarray(image.astype('uint8'), 'RGB')
+    image = image.resize((64, 64))
+    image = np.array(image)
 
-    image = Image.fromarray(image.astype('uint8')) 
-    image = image.resize((128, 128)) 
-    image = np.array(image) / 255.0  
-
-  
-    if image.ndim == 2:
-        image = np.stack((image,)*3, axis=-1)
-
-    prediction = model.predict(image[None, ...])
+    print("Min and max values:", image.min(), image.max())  # Sollte zwischen 0 und 1 sein
+    prediction = model.predict(np.expand_dims(image, axis=0))
     confidences = {labels[i]: float(prediction[0][i]) for i in range(len(labels))}
     return confidences
 
 
-input_image = gr.Image()
-output_text = gr.Textbox(label="Predicted Value")
-
-
+# Gradio interface
 iface = gr.Interface(
     fn=predict_image,
-    inputs=input_image, 
-    outputs=gr.Label(),
-    title="Sentinel Classifier",
-    examples=["images/forest.jpg", "images/highway.jpg", "images/industrial.jpg", "images/residential.jpg", "images/river.jpg"],
-    description="Upload a satellite image and the classifier will predict what it is."
+    inputs=gr.Image(shape=(128, 128)),
+    outputs=gr.Label(num_top_classes=10),
+    title="Sentinel Image Classifier",
+    description="Upload a satellite image and the classifier will predict the type of land cover or feature.",
+    examples=["images/forest.jpg", "images/highway.jpg", "images/industrial.jpg", "images/residential.jpg", "images/river.jpg"]
 )
 
 
